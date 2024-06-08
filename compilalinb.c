f20c6e0366af50df52c34286bf3f1f3a61020cf4
robert <103067656+robert0010@users.noreply.github.com>
2024-06-08 15:05:57 -0300

    leitura de arquivo, implementacao pedidos

--- compilalinb.c
+++ compilalinb.c
@@ -1,14 +1,21 @@
 /* Lucas Ebrenz  2011488 3WC */
 /* Robert Ronald 2020336 3WC */
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "compilalinb.h"
+
+#define MAX 650 //50 linhas no maximo, contando que,
+                //cada linha tenha 13 caracteres no maximo
 
 /* Observacoes
   // Usemos no max 4 "variaveis locais" para resolver o problema
   // No max 2 parametros (p1 e p2)
   // Constantes são escritas na forma $i 
 */
+
 funcp compilaLinB (FILE *f, unsigned char codigo[]){
+  char linha[MAX]; // leitura do arquivo
   // resultado geral
   int resultado = 0 ; // retorno do resultado apos todos os comandos.
   // Atribuicao
@@ -22,21 +29,21 @@ funcp compilaLinB (FILE *f, unsigned char codigo[]){
   char* varpDesvio = "if"; // se encontrada essa string estamos lidando com if
   int num; // número da linha compilador ser desviado caso varp !=0 
   // retorno
-  char* retRetorno = "ret"; // se essa string for lida acabou a funcao 
+  char* ret = "ret"; // se essa string for lida acabou a funcao 
   
-  while (!feof(f) ){ // LOOP PARA LER ARQUIVO
-    fscanf(f,"",  ) ;
-    if (){
-    
+  while (fgets(linha, sizeof(linha), f)){ // LOOP PARA LER ARQUIVO
+    if(strncmp(linha, ret, 3)==0){
+      //execucao do retorno
+    }
+    else if(strncmp(linha, varpDesvio, 2)==0){
+      //execucao de condicional
+    }
+    else{
+      //execucao de operacao
     }
   }
   
-  // valor encontrado apos toda a compilacao
-  return resultado;
+  // nao pode retornar um inteiro e sim um ponteiro para o
+  // inicio da memoria (explicado logo no inicio de implementacao)
+  return !!!;
 }
-
-
-void LiberaFuncao (void *p){
-     // a minima ideia do que fazer nesse bloco.
-}
-
